4415517fb340605b53faeb00ede437ed05134b44
cqtd <cqtd@protonmail.ch>
2021-01-26 18:11:07 +0900

    👓 ready to inject for dx11
    shortcut : <kbd>insert</kbd>
    injector : extreme injector

--- dx11-hook-imgui-menu-base/dx11-hook-imgui-menu-base/framework.h
+++ dx11-hook-imgui-menu-base/dx11-hook-imgui-menu-base/framework.h
@@ -3,3 +3,11 @@
 #define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
 // Windows Header Files
 #include <windows.h>
+
+#include <Windows.h>
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <iomanip>
+#include <unordered_set>
+#include <mutex>
--- dx11-hook-imgui-menu-base/dx11-hook-imgui-menu-base/pch.h
+++ dx11-hook-imgui-menu-base/dx11-hook-imgui-menu-base/pch.h
@@ -1,13 +1,22 @@
-// pch.h: This is a precompiled header file.
-// Files listed below are compiled only once, improving build performance for future builds.
-// This also affects IntelliSense performance, including code completion and many code browsing features.
-// However, files listed here are ALL re-compiled if any one of them is updated between builds.
-// Do not add files here that you will be updating frequently as this negates the performance advantage.
-
 #ifndef PCH_H
 #define PCH_H
 
-// add headers that you want to pre-compile here
 #include "framework.h"
+
+#include "detours.h"
+
+#include "D3D11.h"
+#include <D3Dcompiler.h>
+
+#pragma comment (lib, "D3DCompiler.lib")
+#pragma comment (lib, "D3D11.lib")
+#pragma comment (lib, "winmm.lib")
+#pragma comment (lib, "detours.lib")
+
+#include <imgui.h>
+#include <imgui_impl_win32.h>
+#include <imgui_impl_dx11.h>
+
+using namespace std;
 
 #endif //PCH_H
