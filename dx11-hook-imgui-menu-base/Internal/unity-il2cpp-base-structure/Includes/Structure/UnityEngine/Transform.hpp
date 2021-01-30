aa742b60748182674d3a97d6abe118442bd7ab10
cqtd <cqtd@protonmail.ch>
2021-01-31 01:21:48 +0900

    object

--- dx11-hook-imgui-menu-base/Internal/unity-il2cpp-base-structure/Includes/Structure/UnityEngine/Transform.hpp
+++ dx11-hook-imgui-menu-base/Internal/unity-il2cpp-base-structure/Includes/Structure/UnityEngine/Transform.hpp
@@ -2,9 +2,15 @@
 
 #include "Includes/Structure/UnityEngine/Vector3.hpp"
 #include "Includes/Structure/UnityEngine/Quaternion.hpp"
+#include "Includes/Utils/Macro.h"
 
 namespace UnityEngine
 {
+	constexpr DWORD64 ofnGetPosition = 0x0;
+	constexpr DWORD64 ofnGetRotation = 0x0;
+	constexpr DWORD64 ofnGetChild = 0x0;
+	constexpr DWORD64 ofnGetChildCount = 0x0;
+	
 	struct Transform
 	{
 		// System.Void UnityEngine.Transform::get_position_Injected(UnityEngine.Vector3&)
@@ -18,5 +24,25 @@ namespace UnityEngine
 
 		// System.Int32 UnityEngine.Transform::get_childCount()
 		typedef int fnGetChildCount(Transform* target);
+
+		void GetPosition(Vector3* retPosition)
+		{
+			reinterpret_cast<fnGetPosition*>(BASE_ADDRESS + ofnGetPosition)(this, retPosition);
+		}
+
+		void GetRotation(Quaternion* retQuaternion)
+		{
+			reinterpret_cast<fnGetRotation*>(BASE_ADDRESS + ofnGetRotation)(this, retQuaternion);
+		}
+
+		Transform* GetChild(const int index)
+		{
+			return reinterpret_cast<fnGetChild*>(BASE_ADDRESS + ofnGetChild)(this, index);
+		}
+
+		int GetChildCount()
+		{
+			return reinterpret_cast<fnGetChildCount*>(BASE_ADDRESS + ofnGetChildCount)(this);
+		}
 	};
 }
